b680102588b589ab05da921b2ac185e8706f772c
Karina Song <ksysong@gmail.com>
2021-03-05 03:42:36 +0000

    eslinted the file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,19 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
     for (let i = 0; i < arr1.length; i ++) {
       if (arr1[i] !== arr2[i]) {
-        return false
+        return false;
       }
     }
   }
-  return true; 
-}
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)){
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
     console.log((`💖 Assertion Passed: ${arr1} === ${arr2}`));
   } else {
     console.log(`💀 Assertion Failed: ${arr1} !== ${arr2}`);
   }
-}
+};
