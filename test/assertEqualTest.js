8c2272c8570f6d434f63b3ff1f33a282778f2496
Karina Song <ksysong@gmail.com>
2021-03-08 20:12:41 +0000

    created testing files for head.js and assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+// any other functions used to test this out should be removed and moved to this file 
+
+const assertEqual = require('../assertEqual');
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
