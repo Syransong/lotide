fb1bddb67aabe07b037d0318fd27b095569832d2
Karina Song <ksysong@gmail.com>
2021-03-08 20:11:03 +0000

    refactored to remove assertEqual fcn and added export

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💖💖💖 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr === []) {
@@ -15,8 +9,4 @@ const head = function(arr) {
   }
 };
 
-//Test Code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
+module.exports = head; 
